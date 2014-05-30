cfcff2457c736c7a3f694230c88440038aa78a02
Ravinder Vatish <margaretward@Ravis-Macbook.local>
2014-05-29 21:06:05 -0400

    Initial Commit

---
+++ Dropped/RViewController.h
@@ -0,0 +1,13 @@
+//
+//  RViewController.h
+//  Dropped
+//
+//  Created by Ravinder Vatish on 5/29/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RViewController : UIViewController
+
+@end
---
+++ Dropped/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dropped
+//
+//  Created by Ravinder Vatish on 5/29/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RAppDelegate class]));
+    }
+}
