33d13dbd92bb1c5e7f2df939ee716b15fc8f4759
parmjitkhattra <pama@Pamas-MacBook-Pro.local>
2014-09-15 00:36:37 +0530

    changes by parm

--- Dropped/RViewController.m
+++ Dropped/RViewController.m
@@ -1,29 +1,29 @@
+////
+////  RViewController.m
+////  Dropped
+////
+////  Created by Ravinder Vatish on 5/29/14.
+////  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+////
 //
-//  RViewController.m
-//  Dropped
+//#import "RViewController.h"
 //
-//  Created by Ravinder Vatish on 5/29/14.
-//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//@interface RViewController ()
 //
-
-#import "RViewController.h"
-
-@interface RViewController ()
-
-@end
-
-@implementation RViewController
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-@end
+//@end
+//
+//@implementation RViewController
+//
+//- (void)viewDidLoad
+//{
+//    [super viewDidLoad];
+//	// Do any additional setup after loading the view, typically from a nib.
+//}
+//
+//- (void)didReceiveMemoryWarning
+//{
+//    [super didReceiveMemoryWarning];
+//    // Dispose of any resources that can be recreated.
+//}
+//
+//@end
